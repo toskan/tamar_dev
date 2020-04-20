f4f28ebdd86fb8cc760add58c2213d5ae352f6e7
toskan <tamar.oskanian@gmail.com>
2020-04-20 14:03:59 -0400

    Updates

---
+++ precache-manifest.df665b2822ca661d5bde80409326cf75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17d2b60dc44d575fc49bc172f407bcdf",
+    "url": "/tamar_dev/index.html"
+  },
+  {
+    "revision": "da39a746376283b70805",
+    "url": "/tamar_dev/static/css/main.1b3c88a5.chunk.css"
+  },
+  {
+    "revision": "24997ee9f16e1f701369",
+    "url": "/tamar_dev/static/js/2.446ceeb2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tamar_dev/static/js/2.446ceeb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da39a746376283b70805",
+    "url": "/tamar_dev/static/js/main.6564aeba.chunk.js"
+  },
+  {
+    "revision": "bcbbefb5e35a17e74d37",
+    "url": "/tamar_dev/static/js/runtime-main.0676090a.js"
+  }
+]);
