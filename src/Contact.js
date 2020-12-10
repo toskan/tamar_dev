c1741e3a31d892c77ca657c5b3a69d1f1bd08d72
toskan <tamar.oskanian@gmail.com>
2020-12-10 13:51:46 -0500

    contact form

--- src/Contact.js
+++ src/Contact.js
@@ -1,121 +1,130 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Contact.css';
 // import axios from 'axios';
 
-class Contact extends React.Component {
-	// constructor(props) {
-	// 	super(props);
-	// 	this.state = {
-	// 		name: '',
-	// 		email: '',
-	// 		subject: '',
-	// 		message: '',
-	// 	};
-	// }
-	// onNameChange = (event) => {
-	// 	this.setState({ name: event.target.value });
-	// };
+const Contact = () => {
+	const [inputValues, setInputValues] = useState({
+		name: '',
+		email: '',
+		subject: '',
+		message: '',
+	});
+	const onInputChange = (e) => {
+		setInputValues({ ...inputValues, [e.target.name]: e.target.value });
+	};
 
-	// onEmailChange = (event) => {
-	// 	this.setState({ email: event.target.value });
-	// };
+	const handleSubmit = (e) => {
+		e.preventDefault();
 
-	// onMessageChange = (event) => {
-	// 	this.setState({ message: event.target.value });
-	// };
-	// onSubjectChange = (event) => {
-	// 	this.setState({ subject: event.target.value });
-	// };
-	// resetForm = () => {
-	// 	this.setState({ name: '', email: '', message: '' });
-	// };
-	// //where post request happens
-	// handleSubmit = (e) => {
-	// 	e.preventDefault();
+		if (inputValues.name === '' || inputValues.message === '') {
+			alert('Please Fill All Required Field');
+			return false;
+		}
 
-	// 	axios({
-	// 		method: 'POST',
-	// 		url: 'http://127.0.0.1:3002/send',
-	// 		data: this.state,
-	// 	}).then((response) => {
-	// 		if (response.data.status === 'success') {
-	// 			alert('Message Sent.');
-	// 			this.resetForm();
-	// 		} else if (response.data.status === 'fail') {
-	// 			alert('Message failed to send.');
-	// 		}
-	// 	});
-	// };
-	render() {
-		return (
-			<div className="content-contact">
-				<div className="contact-me">
-					<h3>get in touch</h3>
-				</div>
-				<div className="form-div">
-					<form
-						className="form-contact"
-						action="action_page.php"
-						// onSubmit={this.handleSubmit}
-						method="POST"
-					>
-						<label htmlFor="name" className="visually-hidden">
-							Name:
-						</label>
-						<input
-							type="text"
-							id="name"
-							name="name"
-							placeholder="name"
-							// value={this.state.name}
-							// onChange={this.onNameChange}
-						/>
-						<label
-							htmlFor="exampleInputEmail1"
-							className="visually-hidden"
-						>
-							Email:
-						</label>
-						<input
-							type="email"
-							id="email"
-							name="email"
-							placeholder="email"
-							aria-describedby="emailHelp"
-							// value={this.state.email}
-							// onChange={this.onEmailChange}
-						/>
-						<label htmlFor="subject" className="visually-hidden">
-							Subject:
-						</label>
-						<input
-							type="text"
-							id="subject"
-							name="subject"
-							placeholder="subject"
-							// value={this.state.subject}
-							// onChange={this.onSubjectChange}
-						/>
-						<label htmlFor="message" className="visually-hidden">
-							Write your Message:
-						</label>
-						<textarea
-							id="message"
-							name="message"
-							placeholder="message"
-							rows="5"
-							// value={this.state.message}
-							// onChange={this.onMessageChange}
-						></textarea>
-						<label htmlFor="submit" className="visually-hidden">
-							Send your Message:
-						</label>
-						<input type="submit" value="send" id="submit" />
-					</form>
-				</div>
-			</div>
+		let xmlhttp = new XMLHttpRequest();
+		xmlhttp.open(
+			'POST',
+			'https://cq2cepim09.execute-api.us-east-1.amazonaws.com'
 		);
-	}
-}
+		xmlhttp.setRequestHeader('Content-Type', 'application/json');
+		xmlhttp.send(JSON.stringify(inputValues));
+		xmlhttp.onreadystatechange = function () {
+			if (xmlhttp.readyState === 4) {
+				// let response = JSON.parse(xmlhttp.responseText);
+				if (xmlhttp.status === 200) {
+					console.log('successful');
+					setInputValues('');
+				} else {
+					console.log('failed');
+				}
+			}
+		};
+
+		// axios({
+		// 	method: 'POST',
+		// 	url:
+		// 		'https://wtujjafwfl.execute-api.us-east-1.amazonaws.com/default/sendEmailFunction',
+		// 	data: inputValues,
+		// }).then((response) => {
+		// 	if (response.data.status === 'success') {
+		// 		alert('Message Sent.');
+		// 		setInputValues('');
+		// 	} else if (response.data.status === 'fail') {
+		// 		alert('Message failed to send.');
+		// 	}
+		// });
+	};
+
+	return (
+		<div className="content-contact">
+			<div className="contact-me">
+				<h3>get in touch</h3>
+			</div>
+			<div className="form-div">
+				<form
+					className="form-contact"
+					action="action_page.php"
+					onSubmit={handleSubmit}
+					method="POST"
+				>
+					<label htmlFor="name" className="visually-hidden">
+						Name:
+					</label>
+					<input
+						type="text"
+						id="name name-input"
+						name="name"
+						placeholder="name"
+						value={inputValues.name}
+						onChange={onInputChange}
+					/>
+					<label
+						htmlFor="exampleInputEmail1"
+						className="visually-hidden"
+					>
+						Email:
+					</label>
+					<input
+						type="email"
+						id="email email-input"
+						name="email"
+						placeholder="email"
+						aria-describedby="emailHelp"
+						value={inputValues.email}
+						onChange={onInputChange}
+					/>
+					<label htmlFor="subject" className="visually-hidden">
+						Subject:
+					</label>
+					<input
+						type="text"
+						id="subject subject-input"
+						name="subject"
+						placeholder="subject"
+						value={inputValues.subject}
+						onChange={onInputChange}
+					/>
+					<label htmlFor="message" className="visually-hidden">
+						Write your Message:
+					</label>
+					<textarea
+						id="message description-input"
+						name="message"
+						placeholder="message"
+						rows="5"
+						value={inputValues.message}
+						onChange={onInputChange}
+					></textarea>
+					<label htmlFor="submit" className="visually-hidden">
+						Send your Message:
+					</label>
+					<input type="submit" value="send" id="submit" />
+				</form>
+			</div>
+		</div>
+	);
+};
 
 export default Contact;
+
+// (e) => setInputValues({ name: e.target.value });
--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-// import { css, jsx } from '@emotion/core';
 import images from './images';
 import './portfolio.css';
 import Thumbnail from './Thumbnails';
@@ -7,21 +6,12 @@ import ClickPrompt from './ClickPrompt';
 import PortfolioSlides from './PortfolioSlides';
 import mobile from './mobile';
 
-/**
- * @function Portfolio
- */
 const Portfolio = (props) => {
 	const [nailSelect, setNailSelect] = useState(true);
 
 	const [nailSelected, setNailSelected] = useState('');
 
 	const [slideNumber, setSlideNumber] = useState(0);
-
-	// const mobile =
-	// 	(window.matchMedia('(max-width: 414px)').matches &&
-	// 		window.matchMedia('(orientation: portrait)').matches) ||
-	// 	(window.matchMedia('(max-width: 823px)').matches &&
-	// 		window.matchMedia('(orientation: landscape)').matches);
 
 	useEffect(() => {
 		for (let i = 0; i < images.length; i++) {
