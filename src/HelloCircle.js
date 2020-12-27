1d142dc9cfcca296570657ee991bb33f451d79a6
toskan <tamar.oskanian@gmail.com>
2020-12-26 20:23:24 -0500

    turns

--- src/HelloCircle.js
+++ src/HelloCircle.js
@@ -1,70 +1,58 @@
+import { set } from 'nprogress';
 import React, { useState, useEffect } from 'react';
 
 const HelloCircle = () => {
-	const parev = '\u0562\u0561\u0580\u0565\u0582';
+	// const parev = '\u0562\u0561\u0580\u0565\u0582';
+	// const parev = `\u0532\u0531\u0550\u0535\u0552`;
+	const parev = '\u0532\u0561\u0580\u0565\u0582';
 	const txt = ` ${parev} ${parev} ${parev} ${parev} `.split('');
+	const txtMinusOne = txt.slice(0, txt.length - 1);
 	const deg = 360 / txt.length;
 	const [startOrigin, setStartOrigin] = useState(0);
-	const [degArr, setDegArr] = useState([]);
-	const [elementsPop, setElementsPop] = useState(false);
 
 	useEffect(() => {
 		const letterTimer = setTimeout(() => {
 			setStartOrigin(startOrigin + deg);
-		}, 3950);
+		}, 7900);
 
-		if (elementsPop) {
-			for (let i = 0; i < degArr.length; i++) {
-				document.getElementById(`hello-p-${i}`).animate(
-					[
-						{ transform: `rotate(${startOrigin + deg * i}deg)` },
-						{
-							transform: `rotate(${
-								startOrigin + deg * i + deg
-							}deg)`,
-						},
-					],
+		for (let i = 0; i < txt.length; i++) {
+			document.getElementById(`hello-p-${i}`).animate(
+				[
+					{ transform: `rotate(${startOrigin + deg * i}deg)` },
 					{
-						duration: 4000,
-						iterations: 1,
-					}
-				);
-			}
+						transform: `rotate(${startOrigin + deg * i + deg}deg)`,
+					},
+				],
+				{
+					duration: 8000,
+					iterations: Infinity,
+				}
+			);
 		}
 
 		return () => {
 			clearTimeout(letterTimer);
 		};
-	}, [deg, degArr.length, elementsPop, startOrigin]);
-
-	useEffect(() => {
-		const circularText = (txt, radius, classIndex) => {
-			classIndex = document.getElementsByClassName('hello-circ-text')[
-				classIndex
-			];
-			let value = startOrigin;
-			let origin = value;
-			txt.forEach((ea, index) => {
-				ea = `<p id='hello-p-${index}' style='height:${radius}px;position:absolute;transform-origin:0 100%'>${ea}</p>`;
-				classIndex.innerHTML += ea;
-				setDegArr((degArray) => [...degArray, origin]);
-				origin += deg;
-			});
-			setElementsPop(true);
-		};
-
-		if (!elementsPop) {
-			circularText(txt, 200, 0);
-		}
-	}, [elementsPop, startOrigin, deg, txt]);
+	}, [deg, startOrigin, txt.length]);
 
 	return (
 		<div className="hello-container">
-			<div className="hello-circ-text" id="test"></div>
+			<div className="hello-circ-text" id="test">
+				{txtMinusOne.map((item, i) => (
+					<p
+						id={`hello-p-${i}`}
+						className="hello-p"
+						key={`${i}_${item}`}
+					>
+						{item}
+					</p>
+				))}
+				<p id={`hello-p-${txt.length - 1}`} className="hello-p">
+					{txt[txt.length - 1]}
+				</p>
+			</div>
 		</div>
 	);
 };
 
 export default HelloCircle;
-
-//transform:rotate(${origin}deg);
--- src/Welcome.js
+++ src/Welcome.js
@@ -7,31 +7,55 @@ function Welcome() {
 			<div className="p-intro">
 				<div className="bio-div">
 					<div className="paragraph-div">
-						<p className="bio-paragraphs">
-							I'm Tamar, a Front End Web Developer, based in NY.
-							My programming journey started at General Assembly
-							where I discovered a love for coding. I first
-							completed a Front End Web Development course there
-							and went on to take two other courses: Javascript
-							and React.
+						<div className="bio-paragraphs">
+							<p className="paragraph-bio">
+								I'm Tamar, a Front End Web Developer, based in
+								NY. My programming journey started at General
+								Assembly where I discovered a love for coding. I
+								first completed a Front End Web Development
+								course there and went on to take two other
+								courses: Javascript and React.
+							</p>
 							<br />
+							<p className="paragraph-bio">
+								I built this site in React. My other projects
+								are written in Javascript as well, using jQuery.
+								As a problem solver, making something work makes
+								me happy. And creating something new in the
+								process is a welcome perk.
+							</p>
 							<br />
-							I built this site in React. My other projects are
-							written in Javascript as well, using jQuery. As a
-							problem solver, making something work makes me
-							happy. And creating something new in the process is
-							a welcome perk.
+							<p className="paragraph-bio">
+								Check out my{' '}
+								<Link
+									to="/portfolio"
+									className="portfolio-text-link"
+								>
+									portfolio
+								</Link>{' '}
+								and {` `}
+								<a
+									href="https://github.com/toskan"
+									rel="noopener noreferrer"
+									target="_blank"
+									className="portfolio-text-link"
+								>
+									GitHub
+								</a>
+								{` `}page.
+							</p>
 							<br />
-							<br />
-							Check out my{' '}
-							<Link
-								to="/portfolio"
-								className="portfolio-text-link"
-							>
-								portfolio
-							</Link>{' '}
-							and get in touch if you like what you see.
-						</p>
+							<p className="paragraph-bio">
+								<Link
+									to="/contact"
+									className="portfolio-text-link"
+								>
+									Get in touch
+								</Link>
+								{` `}
+								if you like what you see.
+							</p>
+						</div>
 					</div>
 					<div className="skills-div">
 						<h4 className="skills-h4">SKILLS</h4>
